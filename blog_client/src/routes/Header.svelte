129392f91ea0bd71ff89095117f0b21b83bd538a
David Yang <davidyang438@gmail.com>
2024-07-11 17:39:33 -0700

    real client setup complete, basically ready for just writing

--- blog_client/src/routes/Header.svelte
+++ blog_client/src/routes/Header.svelte
@@ -1,52 +1,69 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
 	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
+		<a href="https://github.com/dyang438">
+			<img src={github} alt="SvelteKit" />
 		</a>
+		<div>
+			<a id="username" href="https://github.com/dyang438">
+				DYANG438
+			</a>
+		</div>
 	</div>
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname.startsWith('/blog') ? 'page' : undefined}>
+				<a href="/blog">Blog</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname.startsWith('/snippets') ? 'page' : undefined}>
+				<a href="/snippets">Snippets</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+	<div class="padding"></div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
+		background-color: aliceblue;
 	}
 
 	.corner {
 		width: 3em;
 		height: 3em;
+		display: flex;
+		color: rgba(0, 0, 0, 1);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+		font-family: aria-current;
+	}
+
+	#username{
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: black;
+	}
+
+	.padding{
+		width: 3em;
+
 	}
 
 	.corner a {
@@ -66,17 +83,7 @@
 	nav {
 		display: flex;
 		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
+		--color-text: black;
 	}
 
 	ul {
---
+++ blog_client/src/routes/blog/cleque/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+import CodeBlock from "../../CodeBlock.svelte";
+
+let deque_init = `\`\`\`c
+#include <stdio.h>
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+\`\`\``;
+
+</script>
+
+<div class="all">
+    <h1>Creation of a Novel Data Structure, Cache Locality Engineered Deque</h1>
+
+    <div> 
+        As my first writing, the creation of this data-structure best captures my intent with this blog. <br><br>
+
+        The Cache Allocated Deque (affectionately called a "cleque") is a data structure I created to solve the nagging seeds of perfectionism in my head. 
+        Data structure choice for cache is often more important than algorithmic complexity, so why are we constantly using queues with possibly random assortments of pointers? <br><br>
+
+        As a result, I propose the cleque. You should not expect it to be: 
+        <ul>
+            <li>groundbreakingly fast</li>
+            <li>memory efficient</li>
+            <li>memory safe</li>
+            <li>thread safe</li>
+            <li>particularly practical in any use case</li>
+        </ul>
+
+        With all of these self-admitted shortcomings, and the cache locality engineered deque destined to fail from its conception, is there still insight to be gained? I certainly believe so.
+
+        <h3>Current Deque Implementations:</h3>
+
+        Here I'm going to be implementing a classic deque in C, and afterwards performing a short investigation of how higher level languages get around the shortcomings of the data structure I build. <br>
+
+        <h2>C Deque with Heap Allocation</h2>
+
+        <!-- Render the Markdown code block -->
+        <div class="code-block">
+            <CodeBlock markdown={deque_init} />
+        </div>
+
+        - possibly look at the source code for the C++ STL deque<br>
+        - possibly look at the source code for the Rust deque<br>
+        - possibly look at the source code for the Python deque<br>
+
+        <h2>The Cache Locality Engineered Deque:</h2>
+    </div>
+</div>
+
+<style>
+    .all {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+    h1 {
+        width: 100%;
+        font-weight: bold;
+    }
+    h2 {
+        width: 100%;
+        line-height: 150%;
+        font-weight: bold;
+        margin-bottom: 0;
+    }
+    .code-block {
+        padding: 5px;
+        border-radius: 5px;
+        overflow-x: auto;
+    }
+</style>
